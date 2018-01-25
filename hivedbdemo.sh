3d70e1b2125492b7ff2f5ec2aace572ea1a4db6a
Bill Gehrke <billgehrke@bgehrke-E862.local>
2018-01-25 08:53:27 -0600

    updted script

--- hivedbdemo.sh
+++ hivedbdemo.sh
@@ -3,15 +3,15 @@
 #
 #pull in data from the edge node
 #
-if [ ! -d "/mapr2" ]
+if [ ! -d "/mapr2" ] then 
 	mkdir /mapr2
 else
 	echo "mapr2 already exists"
 fi
 
 #put the data in its own directory for the hivescripts
-if [ ! -d "/mapr/my.cluseter.com/tmp/dataset"]
-	if [ ! -d "/mapr2/my.cluster.com/tmp/dataset"]
+if [ ! -d "/mapr/my.cluseter.com/tmp/dataset"] then
+	if [ ! -d "/mapr2/my.cluster.com/tmp/dataset"] then
 		mount -o hard,nolock 172.16.9.138:/mapr /mapr2
 		cp -a /mapr2/my.cluster.com/tmp/dataset /mapr/my.cluster.com/tmp
 	else
@@ -19,17 +19,17 @@ if [ ! -d "/mapr/my.cluseter.com/tmp/dataset"]
 	fi
 fi
 
-if [! -d "/mapr/my.cluster.com/tmp/jsonbusiness"]
+if [! -d "/mapr/my.cluster.com/tmp/jsonbusiness"] then 
 	mkdir /mapr/my.cluster.com/tmp/jsonbusiness
 	cp /mapr/my.cluster.com/tmp/dataset/business.json /mapr/my.cluster.com/tmp/jsonbusiness
 fi
 
-if [! -d "/mapr/my.cluster.com/tmp/jsonuser"]
+if [! -d "/mapr/my.cluster.com/tmp/jsonuser"] then
 	mkdir /mapr/my.cluster.com/tmp/jsonuser
 	cp /mapr/my.cluster.com/tmp/dataset/user.json /mapr/my.cluster.com/tmp/jsonuser
 fi
 
-if [ ! -d "mkdir /mapr/my.cluster.com/tmp/jsonreview"]
+if [ ! -d "mkdir /mapr/my.cluster.com/tmp/jsonreview"] then
 	mkdir /mapr/my.cluster.com/tmp/jsonreview
 	cp /mapr/my.cluster.com/tmp/dataset/review.json /mapr/my.cluster.com/tmp/jsonreview
 fi
@@ -60,7 +60,7 @@ echo "Indexes have been built"
 #Move the JSON Serde jar to the right location
 #
 
-if [ ! -f "/opt/mapr/hive/hive-2.1/lib/json-serde-1.3.8-jar-with-dependencies.jar"]
+if [ ! -f "/opt/mapr/hive/hive-2.1/lib/json-serde-1.3.8-jar-with-dependencies.jar"] then
 	cp /gitrepo/hivedbdemo/json-serde-1.3.8-jar-with-dependencies.jar /opt/mapr/hive/hive-2.1/lib
 	echo "Serde Jar has been moved"
 fi
