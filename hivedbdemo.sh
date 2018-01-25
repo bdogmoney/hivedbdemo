6263e36d9f2c518c0485305872a9a280e7f3a2de
Bill Gehrke <billgehrke@bgehrke-E862.local>
2018-01-25 09:20:50 -0600

    updted script

--- hivedbdemo.sh
+++ hivedbdemo.sh
@@ -10,7 +10,7 @@ else
 fi
 
 #put the data in its own directory for the hivescripts
-if [ ! -d "/mapr/my.cluster.com/tmp/dataset"]; then
+if [ ! -d "/mapr/my.cluster.com/tmp/dataset" ]; then
 	if [ ! -d /mapr2/my.cluster.com/tmp/dataset]; then
 		mount -o hard,nolock 172.16.9.138:/mapr /mapr2
 		cp -a /mapr2/my.cluster.com/tmp/dataset /mapr/my.cluster.com/tmp
@@ -19,17 +19,17 @@ if [ ! -d "/mapr/my.cluster.com/tmp/dataset"]; then
 	fi
 fi
 
-if [! -d "/mapr/my.cluster.com/tmp/jsonbusiness"]; then 
+if [ ! -d "/mapr/my.cluster.com/tmp/jsonbusiness" ]; then 
 	mkdir /mapr/my.cluster.com/tmp/jsonbusiness
 	cp /mapr/my.cluster.com/tmp/dataset/business.json /mapr/my.cluster.com/tmp/jsonbusiness
 fi
 
-if [! -d "/mapr/my.cluster.com/tmp/jsonuser"]; then
+if [ ! -d "/mapr/my.cluster.com/tmp/jsonuser" ]; then
 	mkdir /mapr/my.cluster.com/tmp/jsonuser
 	cp /mapr/my.cluster.com/tmp/dataset/user.json /mapr/my.cluster.com/tmp/jsonuser
 fi
 
-if [ ! -d "/mapr/my.cluster.com/tmp/jsonreview"]; then
+if [ ! -d "/mapr/my.cluster.com/tmp/jsonreview" ]; then
 	mkdir /mapr/my.cluster.com/tmp/jsonreview
 	cp /mapr/my.cluster.com/tmp/dataset/review.json /mapr/my.cluster.com/tmp/jsonreview
 fi
@@ -40,25 +40,22 @@ echo "The data has been copied over and put in the right locations"
 #Move the data into mapr-db
 #
 
-if [ ! -f "mapr/my.cluster.com/apps/business"]; then
+if [ ! -f "mapr/my.cluster.com/apps/business" ]; then
 	mapr importJSON -idField business_id -src /mapr/my.cluster.com/tmp/dataset/business.json -dst /apps/business -mapreduce false
 	maprcli table cf edit -path /apps/business -cfname default -readperm p -writeperm p
 	maprcli table index add -path /apps/business -index idx_biz_id -indexedfields business_id
 fi
 
-if [ ! -f "mapr/my.cluster.com/apps/review"]; then
+if [ ! -f "mapr/my.cluster.com/apps/review" ]; then
 	mapr importJSON -idField review_id -src /mapr/my.cluster.com/tmp/dataset/review.json -dst /apps/review -mapreduce false
 	maprcli table cf edit -path /apps/review -cfname default -readperm p -writeperm p
 	maprcli table index add -path /apps/review -index idx_biz_id -indexedfields business_id
 fi 
 
-if [ ! -f "mapr/my.cluster.com/apps/user"]; then
+if [ ! -f "mapr/my.cluster.com/apps/user" ]; then
 	mapr importJSON -idField user_id -src /mapr/my.cluster.com/tmp/dataset/user.json -dst /apps/user -mapreduce false
 	maprcli table cf edit -path /apps/user -cfname default -readperm p -writeperm p
 fi
-
-
-
 
 echo "All the data is in the DB with the authority changed and indexes built"
 
@@ -66,7 +63,7 @@ echo "All the data is in the DB with the authority changed and indexes built"
 #Move the JSON Serde jar to the right location
 #
 
-if [ ! -f "/opt/mapr/hive/hive-2.1/lib/json-serde-1.3.8-jar-with-dependencies.jar"]; then
+if [ ! -f "/opt/mapr/hive/hive-2.1/lib/json-serde-1.3.8-jar-with-dependencies.jar" ]; then
 	cp /gitrepo/hivedbdemo/json-serde-1.3.8-jar-with-dependencies.jar /opt/mapr/hive/hive-2.1/lib
 	echo "Serde Jar has been moved"
 fi
